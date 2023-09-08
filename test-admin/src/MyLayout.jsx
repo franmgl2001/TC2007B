a99f666e047fbf2eabd4730456123de8847e867f
santiagoTena05 <a01781293@tec.mx>
2023-09-07 18:40:38 -0600

    Add logout hook

---
+++ test-admin/src/MyLayout.jsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import { forwardRef } from 'react';
+import { AppBar, Layout, UserMenu, useLogout, ToggleThemeButton} from 'react-admin';
+import { MenuItem } from '@mui/material';
+import ExitIcon from '@mui/icons-material/PowerSettingsNew';
+
+// It's important to pass the ref to allow Material UI to manage the keyboard navigation
+const MyLogoutButton = forwardRef((props, ref) => {
+    const logout = useLogout();
+    const handleClick = () => logout();
+    return (
+        <MenuItem
+            onClick={handleClick}
+            ref={ref}
+            // It's important to pass the props to allow Material UI to manage the keyboard navigation
+            {...props}
+        >
+            <ExitIcon /> Logout
+        </MenuItem>
+    );
+});
+
+const MyUserMenu = () => (
+    <UserMenu>
+        <MyLogoutButton />
+    </UserMenu>
+);
+
+const MyAppBar = () => <AppBar userMenu={<UserMenu />} toolbar={<ToggleThemeButton />} />;
+
+const MyLayout = (props) => (
+    <Layout {...props} appBar={MyAppBar} />
+);
+
+export default MyLayout;
