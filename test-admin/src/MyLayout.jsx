9321826f7a6b134eb85bb2318abd65241194a41d
santiagoTena05 <a01781293@tec.mx>
2023-09-07 18:52:07 -0600

    Add them button  hook

--- test-admin/src/MyLayout.jsx
+++ test-admin/src/MyLayout.jsx
@@ -3,6 +3,8 @@ import { forwardRef } from 'react';
 import { AppBar, Layout, UserMenu, useLogout, ToggleThemeButton} from 'react-admin';
 import { MenuItem } from '@mui/material';
 import ExitIcon from '@mui/icons-material/PowerSettingsNew';
+import { defaultTheme, useTheme } from 'react-admin';
+import { Button } from '@mui/material';
 
 // It's important to pass the ref to allow Material UI to manage the keyboard navigation
 const MyLogoutButton = forwardRef((props, ref) => {
@@ -31,5 +33,15 @@ const MyAppBar = () => <AppBar userMenu={<UserMenu />} toolbar={<ToggleThemeButt
 const MyLayout = (props) => (
     <Layout {...props} appBar={MyAppBar} />
 );
+
+const ThemeToggler = () => {
+    const [theme, setTheme] = useTheme();
+
+    return (
+        <Button onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
+            {theme === 'dark' ? 'Switch to light theme' : 'Switch to dark theme'}
+        </Button>
+    );
+}
 
 export default MyLayout;
