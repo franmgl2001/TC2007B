11bf2466b8833966cb773048891d1ab168449f32
Domingo Mora <A01783317.tec.mx>
2023-10-04 18:01:36 -0600

    Login page stage 1

---
+++ final_proyect/frontend/src/pages/loginpage.jsx
@@ -0,0 +1,44 @@
+import "./loginP.css"
+import Button from '@mui/material/Button';
+import React, { useState } from 'react';
+
+function LoginPage() {
+  // State to store user inputs
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  // Handle form submission
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Add your logic for handling login here, e.g., API calls
+  };
+
+  return (
+    <div className="login-page">
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default LoginPage;
