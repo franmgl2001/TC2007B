93f4470a63813ba5604cc3a102322705bc0cef35
Domingo Mora <A01783317.tec.mx>
2023-10-09 21:47:50 -0600

    Login mejor 4

--- final_proyect/frontend/src/loginpage.jsx
+++ final_proyect/frontend/src/loginpage.jsx
@@ -9,16 +9,11 @@ function LoginPage() {
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
   const login = useLogin()
-  const DashboardButton = () => {
-    const redirect = useRedirect();
-    const handleClick = () => {
-        redirect('/dashboard');
-    }
-    return <button onClick={handleClick}>Dashboard</button>;
-};
+
   // Handle form submission
   const handleSubmit = async (e) => {
     e.preventDefault();
+
 
     // Prepare the request body
     const requestBody = {
@@ -37,6 +32,7 @@ function LoginPage() {
       });
 
       if (response.ok) {
+
           
       } else {
         // User authentication failed
@@ -54,80 +50,49 @@ function LoginPage() {
       flexDirection: 'column', 
       alignItems: 'center', 
       justifyContent: 'center', 
-      minHeight: '100vh', 
+      minHeight: '100vh',
       background: 'linear-gradient(90deg, #007514, #ffffff, #970000)',
     }}>
-      <div style={{ 
-        width: '100%', 
-        maxWidth: '400px', 
-        padding: '30px', 
-        paddingTop: '156px',
-        background: 'white',
-        borderRadius: '5px',
-        boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.2)',
-      }}>
-        <h2 style={{ textAlign: 'center' }}>Login</h2>
-        {error && <p style={{ color: 'red' }}>{error}</p>}
-        <form onSubmit={handleSubmit}>
-          <div style={{ margin: '20px 0' }}>
-            <label style={{ fontWeight: 'bold' }} htmlFor="username">Username:</label>
-            <input
-              type="text"
-              id="username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              style={{ 
-                width: '100%', 
-                padding: '10px', 
-                borderBottom: '2px solid #D1D1D4', 
-                background: 'none', 
-                paddingLeft: '24px', 
-                fontWeight: '700', 
-                transition: '.2s',
-              }}
-            />
-          </div>
-          <div style={{ margin: '20px 0' }}>
-            <label style={{ fontWeight: 'bold' }} htmlFor="password">Password:</label>
-            <input
-              type="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              style={{ 
-                width: '100%', 
-                padding: '10px', 
-                borderBottom: '2px solid #D1D1D4', 
-                background: 'none', 
-                paddingLeft: '24px', 
-                fontWeight: '700', 
-                transition: '.2s',
-              }}
-            />
-          </div>
-          <button
-            type="submit"
-            style={{
-              background: '#fff',
-              fontSize: '14px',
-              marginTop: '30px',
-              padding: '16px 20px',
-              borderRadius: '26px',
-              border: '1px solid #D4D3E8',
-              textTransform: 'uppercase',
-              fontWeight: '700',
-              display: 'flex',
-              alignItems: 'center',
-              width: '100%',
-              color: '#4C489D',
-              boxShadow: '0px 2px 2px #5C5696',
-              cursor: 'pointer',
-              transition: '.2s',
-            }}
-          >
-            Login
-          </button>
-        </form>
+      <div className="login-container">
+        {/* Logo */}
+        <div className="logo">
+          <img src="./fund.png" style={{ width: '100%', height: 'auto' }} />
+        </div>
+        <div className="login-box">
+          <h2 style={{ textAlign: 'center' }}>Login</h2>
+          {error && <p style={{ color: 'red' }}>{error}</p>}
+          <form onSubmit={handleSubmit}>
+            {/* Username input */}
+            <div className="form-group">
+              <label htmlFor="username">Username:</label>
+              <input
+                type="text"
+                id="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                className="login-input"
+              />
+            </div>
+            {/* Password input */}
+            <div className="form-group">
+              <label htmlFor="password">Password:</label>
+              <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="login-input"
+              />
+            </div>
+            {/* Submit button */}
+            <button
+              type="submit"
+              className="login-button"
+            >
+              Login
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
