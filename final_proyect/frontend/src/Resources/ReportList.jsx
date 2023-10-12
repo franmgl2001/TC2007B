b669b830eb23672fe830dcd067dafb194ff262c3
Francisco Martinez Gallardo <franmgl@Franciscos-MacBook-Pro.local>
2023-10-12 17:15:57 -0600

    Get unstash changes

--- final_proyect/frontend/src/Resources/ReportList.jsx
+++ final_proyect/frontend/src/Resources/ReportList.jsx
@@ -1,16 +1,21 @@
 import PriorityChart from '../components/PriorityChart';
 import GroupedClassroomBar from '../components/GroupedClassroomBar';
+import { useState } from 'react';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 
 
-export const ReportList = () => (
-    <div>
-        <h2>My Resource</h2>
-        <GroupedClassroomBar />
-        <PriorityChart />
-    </div>
-);
+export const ReportList = () => {
+    const [Classification, setClassification] = useState("Categoría");
+
+    return (
+        <div>
+            <h2>My Resource</h2>
+            <GroupedClassroomBar dynamicValue={Classification} />
+            <PriorityChart dynamicValue={Classification} />
+        </div>
+    );
+};
 
