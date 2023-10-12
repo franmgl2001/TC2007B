67a44700d6be14baf8f7adeafaab06cd0c66e2b2
santiagoTena05 <a01781293@tec.mx>
2023-10-12 16:37:03 -0600

    Format

--- final_proyect/frontend/src/App.js
+++ final_proyect/frontend/src/App.js
@@ -1,37 +1,37 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import { Admin, Resource, CustomRoutes, Layout, ShowGuesser, EditGuesser } from 'react-admin';
-import { TicketList, TicketCreate } from './Resources/TicketList';
+import { Admin, Resource, CustomRoutes, Layout, ShowGuesser } from 'react-admin';
+import { TicketList, TicketCreate, TicketEdit} from './Resources/TicketList';
 import { dataProvider } from "./dataProvider";
 import Registrarse from "./registrarse";
 import authProvider from './authProvider';
 import LoginPage from './loginpage';
 import { i18nProvider } from './i18nProvider';
 import { MyAppBar } from './components/MyAppBar.js';
-import { UserList, UserCreate } from './Resources/UserList';
+import { UserList, UserCreate, UserEdit } from './Resources/UserList';
 import { ReportList } from './Resources/ReportList';
 import PostIcon from "@mui/icons-material/Book";
 import UserIcon from "@mui/icons-material/Group";
-
-
-
+import BarChartOutlinedIcon from '@mui/icons-material/BarChartOutlined';
 
 
 
 const MyLayout = (props) => <Layout {...props} appBar={MyAppBar} />;
 
+
+
 const App = () => {
   return (
 
 
-    <Admin dataProvider={dataProvider} authProvider={authProvider} layout={MyLayout} i18nProvider={i18nProvider} darkTheme={{ palette: { mode: 'dark' } }}>
+    <Admin dataProvider={dataProvider} authProvider={authProvider} i18nProvider={i18nProvider} layout={MyLayout} darkTheme={{ palette: { mode: 'dark' } }}>
 
 
-      <Resource name="tickets" list={TicketList} create={TicketCreate} show={ShowGuesser} recordRepresentation="Coordinador" edit={EditGuesser} icon={PostIcon} />
+      <Resource name="tickets" list={TicketList} create={TicketCreate} show={ShowGuesser} recordRepresentation="Coordinador" edit={TicketEdit} icon={PostIcon} />
       {permissions => permissions === 'Admin' && (
-        <Resource name="users" list={UserList} create={UserCreate} icon={UserIcon} />
+        <Resource name="users" list={UserList} create={UserCreate} icon={UserIcon} edit={UserEdit}/>
       )}
-      <Resource name="reports" list={ReportList} />
+      <Resource name="reports" list={ReportList} icon={BarChartOutlinedIcon}/>
       <CustomRoutes>
         <Route path="/registrarse" element={<Registrarse />} />
       </CustomRoutes>
--- final_proyect/frontend/src/Resources/TicketList.jsx
+++ final_proyect/frontend/src/Resources/TicketList.jsx
@@ -12,32 +12,40 @@ export const TicketList = () => {
             <Datagrid rowClick="show">
                 {permissions === 'Admin' &&
                     <TextField source="user" label="Coordinador" />}
-                <TextField source="Categoría" />
+                <TextField source="Categoría"/>
                 <TextField source="SubCategoría" />
                 <TextField source="Prioridad" />
                 <TextField source="Aula" />
                 <TextField source="Proceso" />
                 <TextField source="Status" />
                 <TextField source="Fecha de Incidente" />
-                <EditButton basePath="TicketEdit" />
+                <EditButton />
             </Datagrid>
         </List>
     )
 };
 
-export const TicketEdit = () => (
-    <Edit>
-        <SimpleForm>
-            <TextInput source="id" disabled />
-            <TextInput source="coordinador" disabled />
-            <TextInput source="categoria" />
-            <TextInput source="subcategoria" />
-            <TextInput source="status" />
-            <TextInput source="descripcion" />
-            <TextInput source="comentario" multiline rows={5} />
-        </SimpleForm>
-    </Edit>
-);
+export const TicketEdit = () => {
+    const [selectedClasificacion, setSelectedClasificacion] = useState('SubCategoria');
+
+    return(
+        <Edit>
+            <SimpleForm>
+                <div style={{gap: 80, display:'flex'}}>
+                    <TextInput source="id" disabled />
+                    <TextInput source="coordinador" disabled />
+                    <DropDown collection={"Categoría"} setValue={setSelectedClasificacion} Cascade={true} />
+                    <CascadeDropDown collection={selectedClasificacion} parentValue={selectedClasificacion}/>
+                    <DropDown collection={"Status"} setValue={null} Cascade={false} />
+                </div>
+                <div style={{gap: 20, display:'flex'}}>
+                    <TextInput source="descripcion" multiline rows={5} sx={{width:560}}/>
+                    <TextInput source="comentario" multiline rows={5}  sx={{ width: 560}}/>
+                </div>
+            </SimpleForm>
+        </Edit>
+    );
+};
 
 export const TicketCreate = () => {
     const [selectedClasificacion, setSelectedClasificacion] = useState('SubCategoria');
@@ -59,7 +67,7 @@ export const TicketCreate = () => {
                 </div>
 
                 <TextInput source="Proceso" multiline rows={1} sx={{ width: '100%', marginBottom: '10px', marginRight: '1em', '& .MuiFilledInput-input': { paddingTop: '10px' } }} />
-                <TextInput source="Comentario" multiline rows={5} sx={{ width: '100%', marginBottom: '10px', marginRight: '1em', '& .MuiFilledInput-input': { paddingTop: '10px' } }} />
+                <TextInput source="Comentario" multiline rows={4} sx={{ width: '100%', marginBottom: '10px', marginRight: '1em', '& .MuiFilledInput-input': { paddingTop: '10px' } }} />
                 <TextInput source="Resolución" multiline rows={5} sx={{ width: '100%', marginRight: '1em', '& .MuiFilledInput-input': { paddingTop: '10px' } }} />
             </SimpleForm>
         </Create>
